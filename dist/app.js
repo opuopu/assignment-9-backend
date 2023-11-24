759ea5564c6669307bc4967cd8c804942ccf7dd3
opuopu <nurmdopu428@gmail.com>
2023-11-25 02:24:22 +0600

    fix: cors origin issue

--- dist/app.js
+++ dist/app.js
@@ -1,4 +1,13 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -24,6 +33,9 @@ exports.app.use((0, cors_1.default)({
 exports.app.use((0, express_fileupload_1.default)());
 exports.app.use(express_1.default.json());
 exports.app.use(express_1.default.urlencoded({ extended: true }));
+exports.app.get("/", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    res.status(200).send("wow server is working");
+}));
 //All Routes
 exports.app.use("/api/v1", routes_1.default);
 //Global Error Handler
--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 import express, { Application } from "express";
 import cors from "cors";
 import fileUpload from "express-fileupload";
-
+import { Request, Response } from "express";
 import globalErrorHandler from "./app/middlewares/globalErrorHandler";
 import routes from "./app/routes";
 import { NotFoundHandler } from "./errors/NotFoundHandler";
@@ -14,6 +14,7 @@ app.use(
       "http://localhost:3000",
       "https://booking-ideqevieu-opuopu.vercel.app",
       "https://dreamy-narwhal-1b29a1.netlify.app",
+      "https://booking-com-woad.vercel.app/",
     ],
     credentials: true,
   })
@@ -24,6 +25,9 @@ app.use(fileUpload());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.get("/", async (req: Request, res: Response) => {
+  res.status(200).send("wow server is working");
+});
 //All Routes
 app.use("/api/v1", routes);
 
